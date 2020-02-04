426b56ab21c85f6ffa0e040254ffb34e2ed157fa
peterzelenak <peter.zelenak@gmail.com>
2020-02-04 20:39:04 +0100

    Initial commit

---
+++ src/app/hypo-header/hypo-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hypo-header',
+  templateUrl: './hypo-header.component.html',
+  styleUrls: ['./hypo-header.component.css']
+})
+export class HypoHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
